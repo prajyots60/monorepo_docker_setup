8d2154466db3e5a1a8877f2a03bc049aca21b359
Prajyot Shinde <prajyotshinde60@gmail.com>
2025-12-10 20:38:26 +0530

    feat(docker): add Dockerfile for web application with build and run configurations feat(page): enforce dynamic rendering for user data retrieval feat(package): update start script to include database migration

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,9 @@
 import prismaClient from "db/client";
 
+// Force dynamic rendering (no static pre-rendering during build)
+export const dynamic = 'force-dynamic';
 
-export default  async function Home() {
+export default async function Home() {
   const users = await prismaClient.user.findMany();
   return (
     <div>
